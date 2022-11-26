bd4f66dd94b2b8d8821bb03dbc1b7423ba369f14
ChanHeinKyaw <chanheinkyaw.mm@gmail.com>
2022-11-26 12:54:43 +0630

    folder-structure

---
+++ nuxt-js/layouts/custom.vue
@@ -0,0 +1,6 @@
+<template>
+  <div>
+    Custom Nav Goes Here
+    <slot />
+  </div>
+</template>
---
+++ nuxt-js/pages/about.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>This is the about page</div>
+</template> 
+
+
+<script>
+// This will work in both `<script setup>` and `<script>`
+definePageMeta({
+  layout: "custom",
+});
+</script>
