b16bbb620e051604113bd8fbfb918a9192a80f6d
ChanHeinKyaw <chanheinkyaw.mm@gmail.com>
2022-11-26 20:59:29 +0630

    data-fetching

--- nuxt-js/pages/index.vue
+++ nuxt-js/pages/index.vue
@@ -6,6 +6,34 @@
       x: {{ x }},
       y: {{ y }}
     </div>
+
+    <div>
+      <ul v-for="user in users" :key="user.id">
+        <li>{{user.name}}</li>
+      </ul>
+    </div>
+
+    <hr>
+
+    <div>
+      <ul v-for="user in user2" :key="user.id">
+        <li>{{user.name}}</li>
+      </ul>
+    </div>
+
+      <hr>
+
+    <div>
+      <ul v-for="user in user3" :key="user.id">
+        <li>{{user.name}}</li>
+      </ul>
+    </div>
+
+    <div>
+      {{ user.id }}
+      {{ user.name }}
+      {{ user.email }}
+    </div>
   </div>
   
 </template>
@@ -13,4 +41,19 @@
 
 <script setup>
  const {x,y} = useMouse();
+
+ const users = ref([]);
+
+ onMounted(() => {
+  fetch("https://jsonplaceholder.typicode.com/users")
+  .then(response => response.json())
+  .then(data => users.value = data);
+ })
+
+const { data: user2 } = await useAsyncData('user2', () => $fetch('https://jsonplaceholder.typicode.com/users'))
+
+const { data: user3 } = await useFetch('https://jsonplaceholder.typicode.com/users')
+
+const {data: user} = await useFetch('https://jsonplaceholder.typicode.com/users/1',{pick: ['id','name','email']})
+ 
 </script>
